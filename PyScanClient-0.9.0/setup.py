8ece0c2b0d761d0d7dd7bf78c12dd0c6c7a8b61c
qiuyx <qiuyx@ihep.ac.cn>
2015-01-20 10:37:06 -0500

    Adding PyScanClient.

---
+++ PyScanClient-0.9.0/setup.py
@@ -0,0 +1,20 @@
+'''
+Created on 
+
+@author: qiuyx
+'''
+from distutils.core import setup
+setup(name='PyScanClient',
+      version='0.9.0',
+      description='Python Scna Server Client Lib',
+      author='Qiu Yongxiang',
+      author_email='qiuyongxiang05@gmail.com',
+      url = 'http://github.com/pySC',
+      packages= ['ScanClient','Test','example'],
+      classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 2.7',
+     ],
+      install_requires = ['requests>=2.4.3']
+    )
